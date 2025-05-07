164db5a1b8e6fea1c3690b8fe477262f21c375db
José Patrial <jose.patrial@grupo2irmaos.com.br>
2025-05-07 16:32:49 +0000

    Unhandled Runtime Error
    Error: React.Children.only expected to receive a single React element child.
    
    src/components/ui/accordion.tsx (42:5) @ _c4
    
      40 |   const Comp = asChild ? Slot : AccordionPrimitive.Trigger;
      41 |   return (
    > 42 |     <Comp
         |     ^
      43 |       ref={ref}
      44 |       className={cn(
      45 |         "flex flex-1 items-center justify-between py-4 font-medium transition-all",
    Call Stack
    11
    
    Show 4 ignore-listed frame(s)
    _c4
    src/components/ui/accordion.tsx (42:5)
    TripAccordionItem
    src/components/Trips/TripAccordionItem.tsx (134:7)
    <unknown>
    src/components/Trips/Trips.tsx (638:13)
    Array.map
    <anonymous> (0:0)
    Trips
    src/components/Trips/Trips.tsx (637:21)
    AppLayout
    src/components/AppLayout.tsx (164:42)

---
+++ src/main/java/com/example/demo/Service.java
@@ -0,0 +1,13 @@
+
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Service {
+
+    public String getDetails() {
+        return "Service Details";
+    }
+}
+    
